01122975173d5d55c552fbbeb13bc2611ae5ed8a
Jon Nylander <pellepim@gmail.com>
2025-10-11 14:54:00 +0200

    Remove for now unnecessary migration stuff

--- app/dev/users.py
+++ app/dev/users.py
@@ -1,14 +1,22 @@
+#!/usr/bin/env python3
+import logging
+import time
 import uuid
 
+import argh
+import psycopg.errors
+
 import database
+import utils.validate
 
 
 def add_super_admin(tenant_id: uuid.UUID, email: str):
     database.execute(
         tenant_id,
         """
-        insert into users (tenant_id, email, first_name, last_name, role)
-        values (%(tenant_id)s, %(email)s, 'Super', 'Admin', 'super_admin')
+        insert into users (tenant_id, first_name, last_name, role)
+        values (%(tenant_id)s, 'Super', 'Admin', 'super_admin')
         """,
         {"tenant_id": tenant_id, "email": email},
     )
+

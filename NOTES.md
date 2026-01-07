56b4345b0d0237361655d2fef346344f1572e14b
Jon Nylander <pellepim@gmail.com>
2026-01-07 18:22:15 +0100

    Archive SAML Phase 3 as complete
    Move Domain Routing & User Assignment to BACKLOG_ARCHIVE.md.
    Also update dev user emails to acme.com and add sign-in note.

--- NOTES.md
+++ NOTES.md
@@ -3,6 +3,7 @@
 Quick reminders to later build out into full backlog items.
 
 - Rate limit sign in attempts
+- With email based sign-in - verify email address on initial sign-in step.
 - Allow super admins to turn off MFA for a user (effectively resetting to email MFA)
 - Verify all links in all frontend templates lead to actual frontend endpoints
 - Establish a single baseline SQL schema rather than going through all migrations
--- app/dev-docker-entrypoint.sh
+++ app/dev-docker-entrypoint.sh
@@ -14,9 +14,9 @@ echo "Ensuring default tenant"
 python ./dev/tenants.py "$DEV_SUBDOMAIN" 'Development'
 
 echo "Ensuring admin users"
-python ./dev/users.py "$DEV_SUBDOMAIN" super-"$DEV_SUBDOMAIN"@pageloom.com "$DEV_PASSWORD" --role=super_admin --first-name=Super --last-name=Admin
-python ./dev/users.py "$DEV_SUBDOMAIN" admin-"$DEV_SUBDOMAIN"@pageloom.com "$DEV_PASSWORD" --role=admin --first-name=Admin --last-name=User
-python ./dev/users.py "$DEV_SUBDOMAIN" member-"$DEV_SUBDOMAIN"@pageloom.com "$DEV_PASSWORD" --role=member --first-name=Normal --last-name=Member
+python ./dev/users.py "$DEV_SUBDOMAIN" super-"$DEV_SUBDOMAIN"@acme.com "$DEV_PASSWORD" --role=super_admin --first-name=Super --last-name=Admin
+python ./dev/users.py "$DEV_SUBDOMAIN" admin-"$DEV_SUBDOMAIN"@acme.com "$DEV_PASSWORD" --role=admin --first-name=Admin --last-name=User
+python ./dev/users.py "$DEV_SUBDOMAIN" member-"$DEV_SUBDOMAIN"@acme.com "$DEV_PASSWORD" --role=member --first-name=Normal --last-name=Member
 
 echo "Starting app..."
 exec uvicorn main:app --host 0.0.0.0 --port 8000 --reload --app-dir /app --reload-dir /app
